a633760f216aa3297129f3882a4ced5080a3d172
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2020-12-09 00:55:06 +0300

    Начата верификация формы

--- source/js/tabs.js
+++ source/js/tabs.js
@@ -1,16 +1,19 @@
 const tabsLink = document.querySelectorAll(`.tabs__link`);
 const tabsList = document.querySelector(`.tabs__list`);
+const placesLink = document.querySelectorAll(`.places__link`);
 const cards = document.querySelectorAll(`.card`);
 let current = 0;
 
+placesLink.forEach((link, i) => {
+  link.dataset.id = i;
+});
 tabsLink.forEach((link, i) => {
   link.dataset.id = i;
 })
 
 const onClick = (evt) => {
   evt.preventDefault();
-  console.log(evt.target);
-  if(!evt.target.matches(`.link`) || evt.target.dataset.id === current){
+  if (!evt.target.matches(`a`) || evt.target.dataset.id === current) {
     return;
   }
   let newCard = evt.target.dataset.id;
@@ -20,4 +23,6 @@ const onClick = (evt) => {
 }
 
 tabsList.addEventListener(`click`, onClick)
-
+placesLink.forEach(link => {
+  link.addEventListener(`click`, onClick)
+})
