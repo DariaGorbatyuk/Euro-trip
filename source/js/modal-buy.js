8521d286b19a691f6101bd8d33bf086b4a916158
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2020-12-08 21:45:21 +0300

    после покупки успешное сообщение

--- source/js/modal-buy.js
+++ source/js/modal-buy.js
@@ -5,39 +5,37 @@ const newModalBuy = modalBuyTemplate.cloneNode(true);
 const successMessage = document.querySelector(`#success`).content.querySelector(`.success`);
 let close;
 
-const onBtnBuyClick = () => {
+const removeModal = (modal) => {
+  modal.remove();
+  document.removeEventListener(`keydown`, onClose);
+  document.removeEventListener(`click`, onClose);
+}
 
-  const removeModal = (modal) => {
-    modal.remove();
-    document.removeEventListener(`keydown`, onClose);
-    document.removeEventListener(`click`, onClose);
+const onClose = (evt) => {
+  if (evt.target !== close && evt.key !== `Escape` && !evt.target.matches(`.purchase`)) {
+    return;
   }
-
-  const onClose = (evt) => {
-    if (evt.target !== close && evt.key !== `Escape` && !evt.target.matches(`.purchase`)) {
-      return;
-    }
-    removeModal(newModalBuy);
-    removeModal(successMessage)
-  }
-  const onSubmit = (evt) => {
-    evt.preventDefault();
-    debugger;
-    removeModal(newModalBuy);
-    body.insertAdjacentElement(`afterbegin`, successMessage);
-    close = document.querySelector(`.close`);
-    close.addEventListener(`click`, onClose);
-    document.addEventListener(`keydown`, onClose);
-    document.addEventListener(`click`, onClose);
-  }
-
-
-  body.insertAdjacentElement(`afterbegin`, newModalBuy);
-  const formPurchase = document.querySelector(`.purchase__form`);
-  close = document.querySelector(`.close`);
+  removeModal(newModalBuy);
+  removeModal(successMessage)
+}
+const onSubmit = (evt) => {
+  evt.preventDefault();
+  removeModal(newModalBuy);
+  body.insertAdjacentElement(`afterbegin`, successMessage);
+  close = successMessage.querySelector(`.close`);
   close.addEventListener(`click`, onClose);
   document.addEventListener(`keydown`, onClose);
   document.addEventListener(`click`, onClose);
+}
+
+
+const onBtnBuyClick = () => {
+  body.insertAdjacentElement(`afterbegin`, newModalBuy);
+  close = newModalBuy.querySelector(`.close`);
+  close.addEventListener(`click`, onClose);
+  document.addEventListener(`keydown`, onClose);
+  document.addEventListener(`click`, onClose);
+  const formPurchase = document.querySelector(`.purchase__form`);
   formPurchase.addEventListener(`submit`, onSubmit)
 }
 
