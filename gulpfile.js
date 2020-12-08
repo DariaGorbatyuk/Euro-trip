7cd12b0b5125b0808850071dc0e8ff18f5fdda4b
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2020-12-08 20:27:52 +0300

    после покупки успешное сообщение

--- gulpfile.js
+++ gulpfile.js
@@ -42,6 +42,7 @@ gulp.task("server", function () {
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
+  gulp.watch("source/js/*.js", gulp.series("copy", "refresh"));
 });
 
 gulp.task("refresh", function (done) {
@@ -106,5 +107,6 @@ gulp.task("clean", function () {
   return del("build");
 });
 
-gulp.task("build", gulp.series("clean", "images", "webp", "copy", "css", "sprite", "html"));
+gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+// gulp.task("build", gulp.series("clean", "images", "webp", "copy", "css", "sprite", "html"));
 gulp.task("start", gulp.series("build", "server"));
--- source/js/modal-buy.js
+++ source/js/modal-buy.js
@@ -2,25 +2,43 @@ const btnBuy = document.querySelectorAll(`.buy`);
 const body = document.querySelector(`body`);
 const modalBuyTemplate = document.querySelector(`#purchase`).content.querySelector(`.purchase`);
 const newModalBuy = modalBuyTemplate.cloneNode(true);
+const successMessage = document.querySelector(`#success`).content.querySelector(`.success`);
+let close;
 
 const onBtnBuyClick = () => {
-  debugger
-  const onClose = (evt) => {
 
-    if (evt.target !== close && evt.key!==`Escape`&& !evt.target.matches(`.purchase`)) {
-      return;
-    }
-    console.log(evt);
-    newModalBuy.remove();
+  const removeModal = (modal) => {
+    modal.remove();
     document.removeEventListener(`keydown`, onClose);
     document.removeEventListener(`click`, onClose);
   }
 
+  const onClose = (evt) => {
+    if (evt.target !== close && evt.key !== `Escape` && !evt.target.matches(`.purchase`)) {
+      return;
+    }
+    removeModal(newModalBuy);
+    removeModal(successMessage)
+  }
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    debugger;
+    removeModal(newModalBuy);
+    body.insertAdjacentElement(`afterbegin`, successMessage);
+    close = document.querySelector(`.close`);
+    close.addEventListener(`click`, onClose);
+    document.addEventListener(`keydown`, onClose);
+    document.addEventListener(`click`, onClose);
+  }
+
+
   body.insertAdjacentElement(`afterbegin`, newModalBuy);
-  const close = document.querySelector(`.purchase__btn`);
+  const formPurchase = document.querySelector(`.purchase__form`);
+  close = document.querySelector(`.close`);
   close.addEventListener(`click`, onClose);
   document.addEventListener(`keydown`, onClose);
   document.addEventListener(`click`, onClose);
+  formPurchase.addEventListener(`submit`, onSubmit)
 }
 
 btnBuy.forEach(btn => btn.addEventListener(`click`, onBtnBuyClick));
